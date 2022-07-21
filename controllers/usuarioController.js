19df49e11fd146c3cbdaf26e556d6fc8ce618f27
centinela1024tb <centinela1024tb@gmail.com>
2022-07-21 15:26:01 -0500

    [ADD] Se empieza hacer las validaciones con express-validator
    => se usa check para validar el campo
    => encadenando usamos: .notEmpty() | .withMessage('mensaje del error') | .run()
    => Atrapamos los errores en resultado y lo convertimos a un Array resultado.Array()

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../models/Usuario.js';
 
 export const formularioLogin = (req, res) => {
@@ -12,14 +13,20 @@ export const formularioRegistro = (req, res) => {
   });
 }
 
+export const registrar = async(req, res) => {
+  // validacion
+  await check('nombre').notEmpty().withMessage('El nombre es requerido').run(req);
+
+  let resultado = validationResult(req)
+  res.json(resultado.array());
+
+  const usuario = await Usuario.create(req.body);
+  res.json(usuario);
+}
+
 export const formularioOlvidePassword = (req, res) => {
   res.render('auth/olvide-password', {
     pagina: 'Recupera tu acceso a Bienes Raices'
   });
 }
 
-export const registrar = async(req, res) => {
-  const usuario = await Usuario.create(req.body);
-
-  res.json(usuario);
-}
