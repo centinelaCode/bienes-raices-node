d7b92ad144b37cd9c74b44dd06d42641e7eed7be
centinela1024tb <centinela1024tb@gmail.com>
2022-07-21 17:28:33 -0500

    [ADD] Se empieza a configurar el envio de email para el registro

---
+++ helpers/emails.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer'
+
+export const emailRegistro = async(datos) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+}
